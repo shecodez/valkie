071d0f899e8c4d37d01a5d1aa1b6a0f8adc5e8f6
Nicole J. Nobles <shecodez@gmail.com>
2021-05-18 17:47:35 -0400

    feat: realtime-auth state

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,21 @@
 <template>
   <div>
     <h1>HomePage</h1>
-    <Login />
+    <User v-slot:user="{ user }">
+      <div v-if="user">Logged-in as {{ user.uid }}</div>
+      <Login v-else />
+    </User>
   </div>
 </template>
 
 <script>
 import Login from "./Login";
+import User from "./User";
 
 export default {
   components: {
     Login,
+    User,
   },
 };
 </script>

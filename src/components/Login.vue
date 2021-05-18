2e75631fe167c739816b1ea22d52ef11ca771266
Nicole J. Nobles <shecodez@gmail.com>
2021-05-18 19:27:22 -0400

    feat: add login with email and password

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,7 +1,37 @@
 <template>
   <aside>
-    <h3>Sign in Anonymously</h3>
+    <h3>Sign In Anonymously</h3>
     <button @click="auth.signInAnonymously()">Sign In</button>
+
+    <div v-if="isNewUser">
+      <h3>Sign Up for a New Account</h3>
+      <a href="#" @click="isNewUser = false">Returning User?</a>
+    </div>
+    <div v-else>
+      <h3>Sign In with Email</h3>
+      <a href="#" @click="isNewUser = true">Need an Account?</a>
+    </div>
+
+    <label for="email">Email</label><br />
+    <input v-model="email" type="email" placeholder="email" class="input" />
+
+    <label for="password">Password</label><br />
+    <input
+      v-model="password"
+      type="password"
+      placeholder="password"
+      class="input"
+    />
+
+    <button
+      @click="signInOrCreateUser()"
+      class="button"
+      :class="{ 'is-loading': isLoading }"
+    >
+      {{ isNewUser ? "Sign Up" : "Login" }}
+    </button>
+
+    <p v-if="errorMessage" class="has-text-danger">{{ errorMessage }}</p>
   </aside>
 </template>
 
@@ -12,7 +42,33 @@ export default {
   data() {
     return {
       auth,
+
+      isNewUser: false,
+      email: "",
+      password: "",
+
+      isLoading: false,
+      errorMessage: "",
     };
+  },
+
+  methods: {
+    async signInOrCreateUser() {
+      this.isLoading = true;
+      this.errorMessage = "";
+
+      try {
+        if (this.isNewUser) {
+          await auth.createUserWithEmailAndPassword(this.email, this.password);
+        } else {
+          await auth.signInWithEmailAndPassword(this.email, this.password);
+        }
+      } catch (error) {
+        this.errorMessage = error.message;
+      }
+
+      this.isLoading = false;
+    },
   },
 };
 </script>
